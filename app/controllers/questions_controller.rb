c451f663c751cf11d0c1c7fe46a128c388b918b2
ulysse galet <ulysse.galet@rennes-sb.com>
2024-10-08 22:00:51 +0200

    show ok

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,9 @@ class QuestionsController < ApplicationController
     def index
         @questions = Question.all
     end
+
+    def show
+        @questions = Question.find(params[:id])
+    end 
     
 end
