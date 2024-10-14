ce162bfc50f6fbca4dbcf8047a8b27a974aab9d9
ulysse galet <ulysse.galet@rennes-sb.com>
2024-10-14 20:05:35 +0200

    cloudinary OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,22 +21,22 @@ question_3 = Question.create(titre: "Quel animal es-tu", description: "Quel est 
 puts "Création des réponses..."
 
 #Q1
-q1_reponse_1 = Reponse.create(description: "le soleil", bonne_reponse: true, question_id: 1, image:"soleil")
-q1_reponse_2 = Reponse.create(description: "le diamant du botswana", bonne_reponse: false, question_id: 1, image:"Diamant_botswana_aowapf")
-q1_reponse_3 = Reponse.create(description: "La bague d'Estelle", bonne_reponse: false, question_id: 1, image:"bague_mariée_fhes4d")
-q1_reponse_4 = Reponse.create(description: "Valentine", bonne_reponse: false, question_id: 1, image:"valentine_waiphb")
+q1_reponse_1 = Reponse.create(description: "le soleil", bonne_reponse: true, question_id: 1, image:"cs2uypwi9rofbjiwgaq5")
+q1_reponse_2 = Reponse.create(description: "le diamant du botswana", bonne_reponse: false, question_id: 1, image:"mbxiebbj6f1iaawsxwhb")
+q1_reponse_3 = Reponse.create(description: "La bague d'Estelle", bonne_reponse: false, question_id: 1, image:"uzbhjzvftzy4w5utnthu")
+q1_reponse_4 = Reponse.create(description: "Valentine", bonne_reponse: false, question_id: 1, image:"apxb826o7jhmyoqshzwq")
 
 #Q2
-q2_reponse_1 = Reponse.create(description: "Miss France", bonne_reponse: false, question_id: 2, image:"miss_france_tbroun")
-q2_reponse_2 = Reponse.create(description: "Matthis", bonne_reponse: true, question_id: 2, image:"Matthis_jokwtd")
-q2_reponse_3 = Reponse.create(description: "Miss Univers", bonne_reponse: false, question_id: 2, image:"Miss_univers_lwfjeu")
-q2_reponse_4 = Reponse.create(description: "Valentine", bonne_reponse: false, question_id: 2, image:"Valentine_2_fn3fvn")
+q2_reponse_1 = Reponse.create(description: "Miss France", bonne_reponse: false, question_id: 2, image:"ol81jicqztx6jfmxmsji")
+q2_reponse_2 = Reponse.create(description: "Matthis", bonne_reponse: true, question_id: 2, image:"abeqppypxmir7csblrtp")
+q2_reponse_3 = Reponse.create(description: "Miss Univers", bonne_reponse: false, question_id: 2, image:"qd8j6xn4n47qp5tibaaa")
+q2_reponse_4 = Reponse.create(description: "Valentine", bonne_reponse: false, question_id: 2, image:"ajjsltcsl5qmjv3uffmc")
 
 #Q3
-q3_reponse_1 = Reponse.create(description: "Le potit chat", bonne_reponse: false, question_id: 3, image:"potit_chat_fgp1yd")
-q3_reponse_2 = Reponse.create(description: "Le serpent", bonne_reponse: true, question_id: 3, image:"serpent_brcw4k")
-q3_reponse_3 = Reponse.create(description: "La loutre", bonne_reponse: false, question_id: 3, image:"loutre_w3jfgt")
-q3_reponse_4 = Reponse.create(description: "La patate douce", bonne_reponse: false, question_id: 3, image:"patate_douce_ibtseu")
+q3_reponse_1 = Reponse.create(description: "Le potit chat", bonne_reponse: false, question_id: 3, image:"kjhatwekdgkhwrp09hny")
+q3_reponse_2 = Reponse.create(description: "Le serpent", bonne_reponse: true, question_id: 3, image:"erwggca7vq7stdkywdtq")
+q3_reponse_3 = Reponse.create(description: "La loutre", bonne_reponse: false, question_id: 3, image:"v52inlk1miftmzmy2jni")
+q3_reponse_4 = Reponse.create(description: "La patate douce", bonne_reponse: false, question_id: 3, image:"qw8hmspaurxuuwfk4jy0")
 
 
 puts "Fini!... Réponses et questions associées créées"
